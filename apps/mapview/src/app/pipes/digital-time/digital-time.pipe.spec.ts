7d210fd26e49a1b39f84ffdc1c7661e2f515e5cd
Viktor Joba <jobes666@gmail.com>
2023-08-10 15:55:04 +0200

    test: add digital time and dimension pipe unit tests

--- apps/mapview/src/app/pipes/digital-time/digital-time.pipe.spec.ts
+++ apps/mapview/src/app/pipes/digital-time/digital-time.pipe.spec.ts
@@ -1,8 +1,28 @@
 import { DigitalTimePipe } from './digital-time.pipe';
 
 describe('DigitalTimePipe', () => {
+  let pipe: DigitalTimePipe;
+  beforeEach(() => {
+    pipe = new DigitalTimePipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new DigitalTimePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('show time without seconds', () => {
+    expect(pipe.transform(5432, false)).toBe('1:30');
+  });
+
+  it('show time with seconds', () => {
+    expect(pipe.transform(5432, true)).toBe('1:30:32');
+  });
+
+  it('show time without seconds with leading zero in minutes', () => {
+    expect(pipe.transform(3932, false)).toBe('1:05');
+  });
+
+  it('show time with seconds with leading zero in seconds', () => {
+    expect(pipe.transform(5402, true)).toBe('1:30:02');
   });
 });
