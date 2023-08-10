38eaa9b88097b4e9734b1a4b4baaeccfdf71732e
Viktor Joba <jobes666@gmail.com>
2023-08-10 15:55:04 +0200

    test: add digital time and dimension pipe unit tests

--- apps/mapview/src/app/pipes/dimension/dimension.pipe.spec.ts
+++ apps/mapview/src/app/pipes/dimension/dimension.pipe.spec.ts
@@ -1,12 +1,23 @@
 import { TranslocoService } from '@ngneat/transloco';
 
+import { EHeightUnit } from '../../services/open-aip/airport.interfaces';
 import { DimensionPipe } from './dimension.pipe';
 
 describe('DimensionPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DimensionPipe({
+  let pipe: DimensionPipe;
+  beforeEach(() => {
+    pipe = new DimensionPipe({
       translate: (input) => input as string,
     } as TranslocoService);
+  });
+
+  it('create an instance', () => {
     expect(pipe).toBeTruthy();
+  });
+
+  it('show dimension', () => {
+    expect(pipe.transform({ value: 5, unit: EHeightUnit.meter })).toBe(
+      '5shared.altitude.heighUnit.0'
+    );
   });
 });
