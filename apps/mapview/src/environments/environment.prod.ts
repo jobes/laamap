c6563245e687b27a634e6b3bd2e0ff39c887edd1
Viktor Joba <jobes666@gmail.com>
2023-11-26 20:56:29 +0100

    build: fix prod nevironment variable from github variables

--- apps/mapview/src/environments/environment.prod.ts
+++ apps/mapview/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   galleryLicenseKey: process.env['NX_LIGHT_GALLERY_KEY'],
   mapTilesKey: process.env['NX_MAP_TILES_KEY'] ?? 'MISSING_KEY',
   openAipDbUrl: process.env['NX_API_DB_URL'],
-  notamProxy: process.env['NOTAM_PROXY'],
+  notamProxy: process.env['NX_NOTAM_PROXY'],
 };
