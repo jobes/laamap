86966d511b8e9ea118ec66db7a1bf07f3ac580b6
Viktor Joba <jobes666@gmail.com>
2023-11-27 16:13:14 +0100

    feat: integrate logRocket
    closes #35

--- apps/mapview/src/main.ts
+++ apps/mapview/src/main.ts
@@ -3,7 +3,19 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
+import LogRocket from 'logrocket';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  LogRocket.init('mnkoap/laamap');
+
+  const airPlaneName = (JSON.parse(
+    localStorage.getItem('settings.general') || '{}',
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  )?.airplaneName ?? 'unknown') as string;
+  LogRocket.identify(airPlaneName);
+}
 /* eslint-disable no-console */
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
+  console.error(err),
 );
