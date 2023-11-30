7951871316fd08eff4c6f7b9a32eaa0808bfbc0b
Viktor Joba <jobes666@gmail.com>
2023-11-30 12:28:13 +0100

    refactor: change environment production to isDevMode

--- apps/mapview/src/environments/environment.cypress.ts
+++ apps/mapview/src/environments/environment.cypress.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  production: true,
   galleryLicenseKey: 'NX_LIGHT_GALLERY_KEY',
   mapTilesKey: 'NX_MAP_TILES_KEY',
   openAipDbUrl: 'proxy',
--- apps/mapview/src/environments/environment.prod.ts
+++ apps/mapview/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  production: true,
   galleryLicenseKey: process.env['NX_LIGHT_GALLERY_KEY'],
   mapTilesKey: process.env['NX_MAP_TILES_KEY'] ?? 'MISSING_KEY',
   openAipDbUrl: process.env['NX_API_DB_URL'],
