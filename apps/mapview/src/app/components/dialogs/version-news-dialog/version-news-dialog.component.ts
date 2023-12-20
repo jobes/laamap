c87e799e3dd5dae4e2f66f1825f6f97c5ea391b1
Viktor Joba <jobes666@gmail.com>
2023-12-20 13:01:19 +0100

    feat: add version dialog for PWA updated
    news can be viewed in version news dialog that can be showed after PWA app auto upgrade

---
+++ apps/mapview/src/app/components/dialogs/version-news-dialog/version-news-dialog.component.ts
@@ -0,0 +1,38 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  inject,
+} from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { TranslocoModule, TranslocoService } from '@ngneat/transloco';
+import { NgxMarkdownItModule } from 'ngx-markdown-it';
+
+@Component({
+  standalone: true,
+  imports: [
+    TranslocoModule,
+    MatDialogModule,
+    NgxMarkdownItModule,
+    MatButtonModule,
+  ],
+  templateUrl: './version-news-dialog.component.html',
+  styleUrls: ['./version-news-dialog.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class VersionNewsDialogComponent implements OnInit {
+  private transloco = inject(TranslocoService);
+  private data = inject(MAT_DIALOG_DATA) as { [language: string]: string }[];
+  markDownLocalizedMessages: string[] = [];
+
+  ngOnInit(): void {
+    this.markDownLocalizedMessages = this.data
+      .map((langData) => langData[this.transloco.getActiveLang()])
+      .reverse();
+  }
+
+  restart(): void {
+    window.location.reload();
+  }
+}
--- apps/mapview/src/app/services/logger/logger.service.ts
+++ apps/mapview/src/app/services/logger/logger.service.ts
@@ -1,11 +1,95 @@
 /* eslint-disable no-console */
 import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { TranslocoService } from '@ngneat/transloco';
+import { VersionNewsDialogComponent } from '../../components/dialogs/version-news-dialog/version-news-dialog.component';
+import { CookieService } from 'ngx-cookie-service';
+
+interface INewsDef {
+  [version: string]: { [lang: string]: string };
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoggerService {
+  constructor(
+    private readonly updates: SwUpdate,
+    private readonly snackBar: MatSnackBar,
+    private readonly translocoService: TranslocoService,
+    private readonly dialog: MatDialog,
+    private readonly cookieService: CookieService,
+  ) {
+    if (this.updates.isEnabled) {
+      this.logPwaEvents();
+    }
+  }
+
   logErrorMsg(source: string, msg: string): void {
     console.error(source, msg);
+  }
+
+  logPwaEvents(): void {
+    this.updates.versionUpdates.subscribe({
+      next: (evt) => {
+        switch (evt.type) {
+          case 'VERSION_DETECTED':
+            console.log(`Downloading new app version: ${evt.version.hash}`);
+            break;
+          case 'VERSION_READY':
+            console.log(`Current app version: ${evt.currentVersion.hash}`);
+            console.log(
+              `New app version ready for use: ${evt.latestVersion.hash}`,
+            );
+            console.log('app data', evt.latestVersion.appData);
+            this.openSnackbarForVersionReady(evt);
+
+            break;
+          case 'VERSION_INSTALLATION_FAILED':
+            console.log(
+              `Failed to install app version '${evt.version.hash}': ${evt.error}`,
+            );
+            break;
+        }
+      },
+    });
+  }
+
+  private openSnackbarForVersionReady(evt: VersionReadyEvent): void {
+    const news = this.actualNews(evt);
+    this.snackBar
+      .open(
+        this.translocoService.translate('mapView.pwaRestartToUpdate'),
+        news.length
+          ? this.translocoService.translate('mapView.pwaRestartToUpdateDetails')
+          : undefined,
+        { duration: 10000 },
+      )
+      .onAction()
+      .subscribe({
+        next: () =>
+          this.dialog.open(VersionNewsDialogComponent, {
+            width: '100%',
+            data: news,
+          }),
+      });
+  }
+
+  private actualNews(evt: VersionReadyEvent): { [lang: string]: string }[] {
+    const news = ((evt?.latestVersion?.appData as { news: unknown })?.news ||
+      {}) as INewsDef;
+    const versions = Object.keys(news).sort();
+    let currentNews: INewsDef['version'][] = [];
+
+    if (this.cookieService.check('installed_version')) {
+      const lastVersion = this.cookieService.get('installed_version');
+      currentNews = versions
+        .filter((version) => version > lastVersion)
+        .map((version) => news[version]);
+    }
+    this.cookieService.set('installed_version', versions[versions.length - 1]);
+    return currentNews;
   }
 }
