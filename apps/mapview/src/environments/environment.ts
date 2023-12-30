abd146853d0baa5de3b61a778c4bd847d8c37581
Viktor Joba <jobes666@gmail.com>
2023-12-31 00:03:01 +0100

    build: use vite and esbuild

--- apps/mapview/src/environments/environment.ts
+++ apps/mapview/src/environments/environment.ts
@@ -1,19 +1,6 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  galleryLicenseKey: process.env['NX_LIGHT_GALLERY_KEY'],
-  mapTilesKey: process.env['NX_MAP_TILES_KEY'] ?? 'MISSING_KEY',
-  openAipDbUrl: process.env['NX_API_DB_URL'],
-  notamProxy: process.env['NX_NOTAM_PROXY'],
+  galleryLicenseKey: '${NX_LIGHT_GALLERY_KEY}',
+  mapTilesKey: '${NX_MAP_TILES_KEY}',
+  openAipDbUrl: '${NX_API_DB_URL}',
+  notamProxy: '${NX_NOTAM_PROXY}',
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
