1662740677dbf57095647bf2e1757343a8c1343b
Viktor Joba <jobes666@gmail.com>
2023-12-31 00:03:01 +0100

    build: use vite and esbuild

--- apps/mapview/src/environments/environment.cypress.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  galleryLicenseKey: 'NX_LIGHT_GALLERY_KEY',
-  mapTilesKey: 'NX_MAP_TILES_KEY',
-  openAipDbUrl: 'proxy',
-  notamProxy: 'proxy',
-};
---
+++ apps/mapview/src/pounchdb-fix.ts
@@ -0,0 +1,2 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
+(window as any).global = window;
