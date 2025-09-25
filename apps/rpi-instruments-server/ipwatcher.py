1e95e61568b2da6cc4be8e7538d6c6b6119cf651
Viktor Jóba <jobes666@gmail.com>
2025-09-25 17:57:48 +0200

    341 (#344)
    * fix(hardware): add HTTPS check for IP address validation

--- apps/rpi-instruments-server/ipwatcher.py
+++ apps/rpi-instruments-server/ipwatcher.py
@@ -1,12 +1,39 @@
 import socket
 import os
 import time
+import ssl
+import urllib.request
+import urllib.error
+
+def check_https_working(ip_address, port=443, timeout=5):
+    """
+    Check if HTTPS server is working on the given IP address.
+    Returns True if HTTPS is working, False otherwise.
+    """
+    try:
+        # Create SSL context that doesn't verify certificates (since we're testing our own)
+        ssl_context = ssl.create_default_context()
+        ssl_context.check_hostname = False
+        ssl_context.verify_mode = ssl.CERT_NONE
+
+        # Try to establish an HTTPS connection
+        url = f"https://{ip_address}:{port}/"
+        request = urllib.request.Request(url)
+
+        # Set a timeout to avoid hanging indefinitely
+        with urllib.request.urlopen(request, timeout=timeout, context=ssl_context) as response:
+            # If we get here, HTTPS is working (any response code is fine)
+            return True
+
+    except (urllib.error.URLError, urllib.error.HTTPError, socket.timeout, ssl.SSLError, OSError):
+        # Any error means HTTPS is not working properly
+        return False
 
 while True:
     os.chdir('/etc/ssl/private/')
     filename = "omh737.ext"
     myIp = None
-    ipAlreadyChecked = False
+    is_cert_for_ip_working = False
 
     while myIp is None:
         try:
@@ -26,12 +53,12 @@ while True:
     line = f.readline()
     while line:
         if myIp in line:
-            ipAlreadyChecked = True
+            is_cert_for_ip_working = check_https_working(myIp)
             break
         line = f.readline()
 
     f.close()
-    if ipAlreadyChecked:
+    if is_cert_for_ip_working:
         print(myIp, 'found, no changes')
     else:
         print('IP not found, adding to list')
