67bdeb6cae34c745e1f7d3ad0b8a1b57e33d47ec
Viktor Joba <jobes666@gmail.com>
2025-11-15 17:21:34 +0100

    fix(vite): comment out unused plugins and coverage configuration in vite.config.ts

--- apps/mapview/vite.config.ts
+++ apps/mapview/vite.config.ts
@@ -1,12 +1,14 @@
 import angular from '@analogjs/vite-plugin-angular';
-import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+// import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+// import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig(() => ({
   root: __dirname,
   cacheDir: './node_modules/.vite/mapview',
-  plugins: [angular(), nxViteTsPaths() as any, nxCopyAssetsPlugin(['*.md'])],
+  plugins: [
+    angular() /*, nxViteTsPaths() as any, nxCopyAssetsPlugin(['*.md'])*/,
+  ],
   test: {
     name: 'mapview',
     globals: true,
@@ -14,10 +16,10 @@ export default defineConfig(() => ({
     include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     setupFiles: ['src/test-setup.ts'],
     reporters: ['default'],
-    coverage: {
-      provider: 'v8' as const,
-      reportsDirectory: '../../coverage/mapview',
-      include: ['src/**/*.{ts,tsx}'],
-    },
+    // coverage: {
+    //   provider: 'v8' as const,
+    //   reportsDirectory: '../../coverage/mapview',
+    //   include: ['src/**/*.{ts,tsx}'],
+    // },
   },
 }));
